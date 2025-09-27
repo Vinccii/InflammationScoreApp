e2b49f851216a0c0563933f27800b8d9accde8a1
Vincent <vinciheinzelmann@gmail.com>
2025-09-27 11:36:37 +0200

    feat(db): add insert option to save date and quantity in nutrition_logs

--- InflammationScoreApp/Program.cs
+++ InflammationScoreApp/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Data;
 using System.IO;
 using Microsoft.Data.Sqlite;
+using Microsoft.VisualBasic.FileIO;
 
 class Program
 {
+    static string connectionString = @"Data Source=inflammation-score.db";
     static void Main()
     {
-        string connectionString = @"Data Source=inflammation-score.db";
-
+       
         using (var connection = new SqliteConnection(connectionString))
         {
             connection.Open();
@@ -22,6 +24,8 @@ class Program
                 )";
             tableCmd.ExecuteNonQuery();
         }
+
+        GetUserInput();
     }
     
     static void GetUserInput()
@@ -42,9 +46,81 @@ class Program
             
             string command = Console.ReadLine();
 
+            switch (command)
+            {
+                case "0":
+                    Console.WriteLine("\nSee you next time!!\n");
+                    closeApp = true;
+                    Environment.Exit(0);
+                    break;
+                
+                /*case "1":
+                    GetAllStats();
+                    break;*/
+                
+                case "2":
+                    Insert();
+                    break;
+
+                /*case "3":
+                    Delete();
+                    break;
+
+                case "4":
+                    Update();
+                    break;
+                
+                default:
+                    Console.WriteLine("\nInvalid Comand :(\n");
+                    break;*/
+            }
+
+        }
+    }
+    private static void Insert()
+    {
+        string date = GetDateInput();
+
+        int quantity = GetNumberInput("\n\nPlease insert an amount of your messured inflammaitorial food (only integers for now :c)\n\n");
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open(); 
+            var tableCmd = connection.CreateCommand();
+            tableCmd.CommandText =
+                tableCmd.CommandText =
+                $"INSERT INTO nutrition_logs (Date, Quantity) VALUES ('{date}', {quantity});";
+
+
+            tableCmd.ExecuteNonQuery();
+
+            connection.Close();
         }
     }
 
+    internal static string GetDateInput()
+    {
+        Console.WriteLine("\n\nPlease isnert a date in the format of (dd-mm-yy) :). In order to return to the main menu type 0.");
+
+        string DateInput = Console.ReadLine();
+
+        if (DateInput == "0") GetUserInput();
+
+        return DateInput;
+    }
+
+    internal static int GetNumberInput(string message)
+    {
+        Console.WriteLine(message);
+
+        string numberInput = Console.ReadLine();
+
+        if (numberInput == "0") GetUserInput();
+
+        int finalInput = Convert.ToInt32(numberInput);
+
+        return finalInput;
+    }
 }
 
 
